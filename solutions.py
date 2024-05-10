1f5e0803397344b7856db3e74dcb5d93897d2265
Zrossiz <soon.pluxury@mail.ru>
2024-05-10 17:39:54 +0300

    feat: init

---
+++ solutions.py
@@ -0,0 +1,31 @@
+import math
+
+# Найти объем и площадь поверхности прямоугольного параллелепипеда 
+# def get_v(a, b, c):
+#     print('Объем равен: ', a * b * c)
+
+# def get_s(a, b, c):
+#     s = 2 * (a * b + b * c + a * c)
+#     print('Площадь равна: ', s)
+
+# def get_info(a, b, c):
+#     get_s(a, b, c)
+#     get_v(a, b, c)
+
+# get_info(2, 2, 2)
+
+
+# Найди длину окружности и площадь круга по радиусу
+# def get_l(r: int):
+#     l = 2 * math.pi * r
+#     print('Длина окружности равна: ', l)
+
+# def get_s(r: int):
+#     s = math.pi * r ** 2
+#     print('Площадь окружности равна: ', s)
+
+# def get_info(r: int):
+#     get_l(r)
+#     get_s(r)
+
+# get_info(5)
